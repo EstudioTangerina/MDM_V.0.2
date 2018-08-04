5c31e0ff0e6cea5a54e71969077ae81cbf55e9d6
BonnieCoelho <jboni2014@gmail.com>
2018-08-04 20:44:10 -0300

    M.D.M 2.0
    Feito dia : 04/08/2018
    
    Às : 20:42
    
    Feitos:
    1- Correção de mapa
    2- Cutscene layout
    
    :100: :angel:  :love_hotel:
    
    Co-Authored-By: Daniel Henrique <danielshenrique@users.noreply.github.com>
    Co-Authored-By: Bonifacioboni <grandeboni@users.noreply.github.com>

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour {
+
+    //public GameObject SceneCut;
+    public GameObject animObject;
+    public Animator animAnimator;
+    // Use this for initialization
+    void Start () {
+        animAnimator = animObject.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerStay2D(Collider2D coll)
+    {
+        if (coll.gameObject.name == "Body + Head")
+        {
+            animAnimator.SetBool("Cutscene", true);
+        }
+        /*if (coll.gameObject.name == "Body + Head")
+        {
+            SceneCut.SetActive(true);
+            print("Ta funfando bro");
+        }*/
+
+
+    }
+}
